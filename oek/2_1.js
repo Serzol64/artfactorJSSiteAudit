776729a5205619eccee8e1a0ac8b0f3aa3ec3e18
Сергей Золотарёв <zolotaryow.mswebdev@outlook.com>
2023-11-09 12:45:09 +0400

    My first in life test v1.5

---
+++ oek/2_1.js
@@ -0,0 +1,25 @@
+var links = document.links;
+for (var i = 0; i < links.length; i++) {
+  var linkHref = links[i].href,
+	  getExt = get_url_extension(linkHref);
+  
+  let isDocument = (getExt === 'doc' || getExt === 'docx') || (getExt === 'pdf' || getExt === 'zip') || (getExt === 'xlsx' || getExt === 'xls');
+  
+  if(isDocument){
+	  fetch(linkHref).then(afterSuccessScan).catch(afterFailedScan);
+  }
+		
+}
+
+function afterSuccessScan(response){
+	if(!response.ok){ 
+		console.error(response.url + '\n');  
+	}
+}
+function afterFailedScan(error){
+	console.error(error.url + '\n'); 
+}
+
+function get_url_extension( url ) {
+    return url.split(/[#?]/)[0].split('.').pop().trim();
+}
